69496dc9f10cf63ab607b102fb56e8a934752cca
sumandas473 <dass52120@gmail.com>
2024-04-21 16:55:16 +0530

    little

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -16,10 +16,10 @@ function Home() {
                 <h1 className='text-[25em] text-white  font-bold  pb-28' id='hi'>HI</h1>
                 <div className=' h-[25vw] w-[22vw] border-2  rotate-6 rounded-[40px] overflow-hidden' id='bg-photo'><img src={Profile} alt="" className='h-full w-full object-cover' /></div>
 
-                <div className='h-[80%] w-[50%] absolute left-0 py-32 pl-20  ' id='left-sec'>
+                <div className='h-[80%] w-[50%] absolute left-0 py-32 pl-20 flex flex-col justify-center ' id='left-sec'>
                     <h1 className='text-[3.5em] font-semibold  text-white' id='text-title'>I am Suman</h1>
                     <h1 className='text-[6em]  capitalize font-bold from-purple-600 via-pink-600 to-blue-600 bg-gradient-to-r bg-clip-text text-transparent  ' id='text-subtitle'>web developer +</h1>
-                    <h1 className='text-[6em]  capitalize font-bold from-purple-600 via-pink-600 to-blue-600 bg-gradient-to-r bg-clip-text text-transparent ' id='text-subtitle'>UI/UX designer</h1>
+                    <h1 className='text-[6em] mt-[-40px] capitalize font-bold from-purple-600 via-pink-600 to-blue-600 bg-gradient-to-r bg-clip-text text-transparent ' id='text-subtitle'>UI/UX designer</h1>
                     <p className=' text-lg  capitalize  text-white my-4 opacity-50 ' id='text-p'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero magnam numquam quas, quae optio facere tempora beatae nam ex error illum asperiores, pariatur enim culpa perspiciatis dignissimos ducimus. Enim numquam ad deleniti facere! Reprehenderit quae doloremque placeat illo magnam autem!</p>
                     <div className='  py-10 flex justify-around w-[50%] ' id='text-bg'>
                         <a href={CV}  download className='px-8 py-3 text-white rounded-[50px] bg-[#540185]  hover:bg-[#9b3ad3] hover:scale-105' id='text'>Download CV</a>
